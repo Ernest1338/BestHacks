73cb475ee24e72b401fe26855dec2391208e6b25
Foxyg3n <piotrek.suchecki@vp.pl>
2023-10-29 01:41:29 +0200

    test endpoint

--- frontend/src/routes/test/+page.svelte
+++ frontend/src/routes/test/+page.svelte
@@ -1,6 +1,19 @@
 <script>
+	import { backend } from "$lib";
 	import Filters from "$lib/components/Filters.svelte";
 	import Searchbar from "$lib/components/Searchbar.svelte";
+	import { onMount } from "svelte";
+
+	onMount(async () => {
+		let result = await backend.post("userregister", "martin", {
+			email: "",
+			name: "sdsd",
+			surname: "",
+			password: "",
+			phone: ""
+		});
+		console.log(JSON.parse(result));
+	});
 </script>
 
 <Searchbar />
