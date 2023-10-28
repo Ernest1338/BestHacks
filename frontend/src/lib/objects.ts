01e533fed9148886a667c080ad492a15562fdc13
Foxyg3n <piotrek.suchecki@vp.pl>
2023-10-28 13:45:04 +0200

    Added login and registration objects

--- frontend/src/lib/objects.ts
+++ frontend/src/lib/objects.ts
@@ -1,4 +1,29 @@
-export interface User {
+export interface UserRegister {
     readonly name: string;
+    readonly surname: string;
+    readonly email: string;
+    readonly password: string;
+    readonly tel: string;
+}
+
+export interface UserLogin {
+    readonly email: string;
+    readonly password: string;
+}
+
+export interface CompanyRegister {
+    readonly nip: string;
+    readonly regon: string;
+    readonly nipsc: string;
+    readonly regonsc: string;
+    readonly krs: string;
+    readonly pkd: string;
+    readonly compname: string;
+    readonly name: string;
+    readonly surname: string;
+}
+
+export interface CompanyLogin {
+    readonly email: string;
     readonly password: string;
 }
