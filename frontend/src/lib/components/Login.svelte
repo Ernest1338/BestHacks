ce12327aa5ae3c3222266a8c2d95fdcc27b84436
GrzegorzSzczepanek <grzegorzszczepanek48@gmail.com>
2023-10-28 14:05:03 +0200

    login

---
+++ frontend/src/lib/components/Login.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import { backend } from "$lib";
+	import type { UserLoginData } from "$lib/objects";
+
+    // todo imie nazwisko, email, *nr tel, 
+    // ToDo
+    let loginData: UserLoginData = {
+		email: "",
+		password: ""
+	}
+    
+    function sendLoginData() {
+        backend.post("/userlogin", loginData )
+    }
+
+
+</script>
+
+<form>
+    <label for="email">Adres mailowy</label>
+    <input type="email" name="email" bind:value={loginData.email}>
+    <label for="password">Hasło</label>
+    <input type="password" name="password" bind:value={loginData.password}>
+    <button type="submit" on:click={sendLoginData}>Zaloguj się</button>
+</form>
+
+<style>
+
+form {
+    width: 600px;
+    height: 800px;
+}
+
+</style>
---
+++ frontend/src/routes/(forms)/+layout.svelte
@@ -0,0 +1,11 @@
+<section class="app">
+<slot></slot>
+</section>
+
+
+<style>
+    .app {
+        width: 100%;
+        height: 100%;
+    }
+</style>
