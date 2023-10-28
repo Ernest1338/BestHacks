b846ecfb6a1a1010534633c49840c132c42a8c06
Ernest Gupik <ernestgupik@wp.pl>
2023-10-29 01:39:41 +0200

    frontend: main size improvements

--- frontend/src/lib/components/Footer.svelte
+++ frontend/src/lib/components/Footer.svelte
@@ -1,11 +1,22 @@
 <script>
-
-
+import Fa from 'svelte-fa'
+import { faHome, faEnvelope, faCodeBranch, faWheelchair, faCrutch } from '@fortawesome/free-solid-svg-icons'
+import { faGithub, faLinkedin, faSpaceAwesome } from '@fortawesome/free-brands-svg-icons';
 </script>
 
 <footer>
-    <section id="icon-section"></section>
-    <span>Made by Gwiezdna Flota 2023</span>
+    <section id="icon-section">
+        <a href="/">
+            <Fa icon={faHome}/> Główna
+        </a>
+        <a href="https://pfron.org.pl/instytucje/">
+            <Fa icon={faCrutch}/> Państwowy Fundusz Rehabilitacyji Osób Niepełnosprawnych
+        </a>
+        <a href="https://www.gov.pl/web/rodzina/osoby-niepelnosprawne">
+            <Fa icon={faWheelchair}/> Państwowy Informator Dla Osób Niepełnosprawnych
+        </a>
+    </section>
+    <span>Gwiezdna Flota &nbsp;<Fa icon={faSpaceAwesome}/>&nbsp; 2023</span>
 </footer>
 
 <style lang="scss">
@@ -18,5 +29,11 @@
         padding: 10px;
         color: $text;
         font-weight: 600;
+    }
+    a {
+        text-decoration: none;
+        color: $text;
+        margin-left: 10px;
+        margin-right: 10px;
     }
 </style>
