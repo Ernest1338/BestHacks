535d3273934e4c5ff75bb47b36b891f6152c66e7
Foxyg3n <piotrek.suchecki@vp.pl>
2023-10-28 16:32:29 +0200

    fixed side menu

--- frontend/src/lib/components/SideMenu.svelte
+++ frontend/src/lib/components/SideMenu.svelte
@@ -1,22 +1,15 @@
 <script>
-	import { goto } from "$app/navigation";
-    import { cookies } from "$lib";
+	import { loginutil } from '$lib';
     import { fly } from 'svelte/transition';
 
-    // function logOut() {
-    //     cookies.delete("loggedAs")
-    //     cookies.delete("authkey")
-    //     goto("/");
-    //     location.reload();
-    // }
+    // loginutil.logout();
 </script>
 
 
-<nav class="menu"   in:fly={{ x: 500, delay: 0, duration: 300 }} out:fly={{ x: 500, duration: 300 }}>
+<nav class="menu"   in:fly={{ x: 300, delay: 0, duration: 300 }} out:fly={{ x: 300, duration: 300 }}>
     <a class="menu-element" href="/user-profile">Profil</a>
     <a class="menu-element" href="/login">Zaloguj się</a>
     <a class="menu-element" href="/register">Zarejestruj się</a>
-
 </nav>
 
 <style lang="scss">
@@ -28,8 +21,9 @@
     right: 0;
     top: 0;
     height: 100vh;
-    width: 500px;
-    border: 2px solid black;
+    width: 300px;
+    border-radius: 10px 0 0 10px;
+    box-shadow: 0 0 10px black;
     display: flex;
     flex-direction: column;
     justify-content: center;
